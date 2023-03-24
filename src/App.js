fe6384d4df2ed48c25add2caed7b5c1ba18e763d
Amol shelke <shelkeamol897@gmail.com>
2023-03-24 15:52:05 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="flex justify-center h-screen py-4">
-      <div className="text-4xl text-black font-bold shadow-sm">
-        <h1>Movie Search</h1>
+    <div className="flex justify-center h-screen py-4 bg-gray-200">
+      <div>
+        <h1 className="text-4xl text-gray-800 font-bold shadow-sm">
+          Movie Schedule
+        </h1>
       </div>
     </div>
   );
