14e8f8bdc60be85df934ab7fe5a3b47f8aba9bd8
Amol shelke <shelkeamol897@gmail.com>
2023-03-24 15:23:38 +0530

    Configured Tailwind and added custom fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex justify-center h-screen py-4">
+      <div className="text-4xl text-black font-bold shadow-sm">
+        <h1>Movie Search</h1>
+      </div>
+    </div>
+  );
 };
 
 export default App;
