aeeada3d803d6a89bb26eaee557848c412b36b10
Amol shelke <shelkeamol897@gmail.com>
2023-03-24 23:18:31 +0530

    created movie search form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="flex justify-center h-screen py-4 bg-gray-200">
+    <div className="flex text-center justify-center h-screen py-4 bg-[#5A5A5A]">
       <div>
-        <h1 className="text-4xl text-gray-800 font-bold shadow-sm">
+        <h1 className="text-4xl text-white font-bold shadow-sm">
           Movie Schedule
         </h1>
+        <form className="p-4">
+          <input
+            type="text"
+            placeholder="Enter Movie Name"
+            className="bg-white text-black font-bold rounded-md py-4 px-8 shadow-sm w-[300px] focus:outline-none"
+          />
+        </form>
       </div>
     </div>
   );
