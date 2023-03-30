e47d3dcb40708dfdc74cdcc0823a21b2df70e09f
Amol shelke <shelkeamol897@gmail.com>
2023-03-31 00:28:45 +0530

    Update search feature and UI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    getMovies("Batman");
+    getMovies("Naruto");
   }, []);
 
   const getMovies = async (movieName) => {
@@ -17,14 +17,19 @@ const App = () => {
     setMovies(data.Search);
   };
 
-  console.log(movies);
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    getMovies(movieSearchInput);
+    setMovieSearchInput("");
+  };
 
   return (
     <div className="h-full py-4 bg-gradient-to-tr from-[#2193b0] to-[#ffc3a0]">
       <div className="flex justify-center flex-col text-center">
         <h1 className="text-4xl text-white font-bold">Movie Schedule</h1>
         <div className="mb-8">
-          <form className="p-4" onSubmit={() => getMovies(movieSearchInput)}>
+          <form className="p-4" onSubmit={formSubmitHandler}>
             <input
               type="text"
               placeholder="Enter Movie Name"
@@ -35,7 +40,7 @@ const App = () => {
 
             <button
               type="submit"
-              onSubmit={() => getMovies(movieSearchInput)}
+              onSubmit={formSubmitHandler}
               className="ml-2 bg-[#2193b0] shadow-md text-white px-6 py-2 rounded-md">
               Search
             </button>
@@ -53,26 +58,24 @@ const App = () => {
               className="h-[520px] bg-[#6e3530] rounded-md text-white w-[400px]">
               <div>
                 <img
-                  src={movie.poster}
+                  src={movie.Poster}
                   alt="/"
-                  className="h-[300px] w-full top-0 left-0 rounded-md"
+                  className="h-[400px] w-full top-0 left-0 rounded-md"
                 />
               </div>
-              <div className="flex justify-between items-center text-center h-[50px] px-6 py-2 mb-1">
-                <h3 className="text-sm font-bold">{movie.original_title}</h3>
+              <div className="flex flex-col justify-start items-start text-left pt-4 mb-1 px-2">
+                <h3 className="text-sm font-bold">{movie.Title}</h3>
                 <p className="text-center">
-                  Rating{" "}
+                  imdbID {""}
                   <span className="font-bold text-yellow-200">
-                    {movie.vote_average}
+                    {movie.imdbID}
                   </span>
                 </p>
               </div>
-              <div className="flex flex-col text-left px-6 py-2 items-start">
-                <p className="text-sm overflow-hidden h-[80px] mb-2">
-                  {movie.overview}
-                </p>
-                <p className="text-md">Release Date: {movie.release_date}</p>
-              </div>
+
+              <p className="text-sm px-2 mt-1 absolute">
+                Released year: {movie.Year}
+              </p>
             </div>
           ))}
       </div>
